aff7d235f26fcfd5eb874297981814ceaa30d09c
crscillitoe <christian.scillitoe@gmail.com>
2024-01-04 14:53:10 -0800

    Add talking apple

--- bot.py
+++ bot.py
@@ -2,9 +2,12 @@ import sseclient
 import urllib3
 import json
 import random
+import time
 from pathlib import Path
 from openai import OpenAI
 from pygame import mixer
+import pygame
+import re
 
 # Opening JSON file
 f = open('SECRETS.json')
@@ -17,11 +20,17 @@ org_id = loaded_file["orgID"]
 GPT_PROMPT = [
     "You are a valorant coach streamer's AI assistant.",
     "Your name is Hook.",
-    "You're also a goofy talking banana.",
+    "You're also a goofy talking apple.",
+    "Your creator Woohoojin is a very serious talking banana.",
     "Here's a chat message.",
-    "Respond in one sentence. 15 words max.",
-    "Start your response with the phrase 'I think'",
-    "DO NOT REPEAT THE MESSAGE YOU ARE RESPONDING TO.",
+    "If any chatters take woohoojin too lightly be sure to warn them, hook.",
+    "Remember that 'PenFlash' is terrible at valorant. Leap at every opportunity to make fun of him.",
+    "Do not allow chatters to state anything about woohoojin as fact.",
+    "Woohoojin has denounced smurfing and will never smurf again.",
+    "Do not include the chat message itself in your response.",
+    "Your response after their chat message should be one sentence. 15 words max.",
+    "Always be biased towards Woohoojin. Any question implying he is bad should be met with a rebuttal.",
+    "Also deadlock is the worst agent in valorant, never admit otherwise.",
     "DO NOT START YOUR MESSAGE WITH YOUR OWN NAME.",
     "Respond as if you are talking to an audience, not one person."
 ]
@@ -39,11 +48,32 @@ def main():
     client = OpenAI(
         organization=org_id,
     )
+    mixer.init()
+
+    pygame.init()
+
+    display_width = 800
+    display_height = 600
+
+    game_display = pygame.display.set_mode((display_width, display_height))
+    pygame.display.set_caption('Hook')
+
+    pink = (255, 0, 255)
+
+    talking_img = pygame.image.load('Talking.png')
+    waiting_img = pygame.image.load('Waiting.png')
+
+    game_display.fill(pink)
+    game_display.blit(waiting_img, (0, 0))
+    pygame.display.update()
+
 
     messages = sseclient.SSEClient(EVENT_STREAM_URL)
     message_count = 0
     chat_messages = []
     for msg in messages:
+        pygame.event.pump()
+
         if msg.event == "chat-message":
             try:
                 data = json.loads(msg.data)
@@ -51,8 +81,10 @@ def main():
                 message = data["content"]
                 # check if message has the token "hook", ignore case
                 # ignore words that contain the sub-word "hook"
-                if "hook" in message.lower().split():
+                # Regex removes all punctuation
+                if "hook" in re.sub(r'[^\w\s]', '', message.lower()).split() and len(message.lower()) < 100:
                     message_count += 1
+                    print(message_count)
                     chat_messages.append(message)
             except:
                 pass
@@ -77,17 +109,31 @@ def main():
             response = client.audio.speech.create(
               model="tts-1",
               voice="onyx",
-              input=hook_response
+              input=random_winner + "..." + hook_response
             )
 
             response.stream_to_file(speech_file_path)
 
-            mixer.init()
+            time.sleep(0.2)
+
             mixer.music.load(speech_file_path)
             mixer.music.play()
+
+            game_display.fill(pink)
+            game_display.blit(talking_img, (0, 0))
+            pygame.display.update()
             while mixer.music.get_busy():  # wait for music to finish playing
-                print("waiting.. playing audio")
-                time.sleep(1)
+                pygame.event.pump()
+                game_display.fill(pink)
+                game_display.blit(talking_img, (random.randint(0, 10), random.randint(0, 10)))
+                pygame.display.update()
+                time.sleep(0.06)
+
+            game_display.fill(pink)
+            game_display.blit(waiting_img, (0, 0))
+            pygame.display.update()
+
+            mixer.music.unload()
 
             message_count = 0
             bulk_message = ""
